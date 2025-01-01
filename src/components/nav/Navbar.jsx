a24de2754f47310dcb970c32842316476f6547d7
hasanisokay <hasanisokay@gmail.com>
2025-01-01 09:19:33 +0600

    added scrollable nav

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,11 +8,32 @@ import logOut from "@/utils/logOut.mjs";
 import Image from "next/image";
 import logo from "./../../../public/hazi_harun_logo1.png";
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const currentPath = usePathname()
   const route = useRouter();
-  const toggleMenu = () => setIsOpen(!isOpen);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
   const user = useSelector(state => state.user.userData)
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+        setVisible(false);
+        setMenuOpen(false)
+    } else {
+        setVisible(true);
+    }
+    setLastScrollY(window.scrollY);
+};
+
+useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [lastScrollY]);
+
   const handleLogOut = () => {
     logOut()
     route.replace(`/login?redirectTo=${currentPath}`)
@@ -59,16 +80,20 @@ const Navbar = () => {
     </svg>}
   </button>
   return (
-    <div className="dark:bg-[#0c0c0e]  sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border shadow-lg">
-      <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8" aria-label="Global">
+    <div className="dark:bg-[#0c0c0e]  sticky top-0 z-50 w-full bg-gray-700 supports-[backdrop-filter]:bg-background/60 dark:border-border shadow-lg">
+      {/* <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"> */}
+      <nav           className={`fixed top-0 left-0 w-full  mx-auto px-2 sm:px-6 lg:px-8 z-50 transition-transform
+                bg-opacity-85 backdrop-blur-sm 
+                duration-300 ${visible ? 'transform-none border-b border-border/40 shadow-md' : '-translate-y-full'
+                }`}  aria-label="Global">
         <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
             <button
               type="button"
               onClick={toggleMenu}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
-              aria-expanded={isOpen ? "true" : "false"}
+              aria-expanded={menuOpen ? "true" : "false"}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -140,10 +165,13 @@ const Navbar = () => {
       </nav>
 
       {/* Mobile menu */}
-      <div className={`${isOpen ? "block" : "hidden"} sm:hidden`} id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <div
+  className={`${menuOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"} duration-500 transition-all absolute top-[64px] right-0 w-[200px] sm:hidden ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-300'} rounded-lg shadow-lg backdrop-blur-lg`}
+  id="mobile-menu"
+>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 ">
           <Link href="/" className={getLinkClass("/")}>হোম</Link>
-          {user && <div className={`${currentPath.includes('/deposits')
+          {user && <div className={`flex items-center justify-start ${currentPath.includes('/deposits')
             ? "text-white bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
             : "dark:text-white text-black dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"}
        flex gap-4 `}>
@@ -154,7 +182,7 @@ const Navbar = () => {
               +
             </Link>
           </div>}
-          {user && <div className={`${currentPath.includes('/members')
+          {user && <div className={`flex items-center justify-start ${currentPath.includes('/members')
             ? "text-white bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
             : "dark:text-white text-black dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"}
        flex gap-4 `}>
@@ -165,7 +193,7 @@ const Navbar = () => {
               +
             </Link>
           </div>}
-          {user && <div className={`${currentPath.includes('/projects')
+          {user && <div className={`flex items-center justify-start ${currentPath.includes('/projects')
             ? "text-white bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
             : "dark:text-white text-black dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"}
        flex gap-4 `}>
@@ -176,7 +204,9 @@ const Navbar = () => {
               +
             </Link>
           </div>}
-          {user && <Link href="/profile" className={getLinkClass("/profile")}>প্রোফাইল</Link>}
+          <div className="">
+            {user && <Link href="/profile" className={getLinkClass("/profile")}>প্রোফাইল</Link>}
+          </div>
           {user && <button className="dark:text-white text-black px-3 py-2 rounded-md text-sm font-medium" onClick={handleLogOut}>লগ আউট</button>}
           <p>
             {themeSwitch}
