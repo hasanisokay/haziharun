141ae81e6ffd6d6a31d904c2295a5fb5b06ebfa7
hasanisokay <hasanisokay@gmail.com>
2025-01-03 21:59:48 +0600

    added react-date-picker

--- src/components/datepicker/DatePicker.jsx
+++ src/components/datepicker/DatePicker.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
 const CustomDatePicker = ({ showDateIcon = true, onDateChange, placeholder, width, defaultValue, classNames }) => {
     const [selectedDate, setSelectedDate] = useState(null);
 
@@ -9,7 +9,7 @@ const CustomDatePicker = ({ showDateIcon = true, onDateChange, placeholder, widt
         if (defaultValue) {
             setSelectedDate(new Date(defaultValue));
         } else {
-            setSelectedDate(new Date())
+            setSelectedDate(new Date());
         }
     }, [defaultValue]);
 
@@ -19,47 +19,47 @@ const CustomDatePicker = ({ showDateIcon = true, onDateChange, placeholder, widt
     };
 
     return (
-        <div style={{ width: width || "40px" }} className={classNames}>
-            {showDateIcon ? <DatePicker
-                showIcon
-                selected={selectedDate}
-                onChange={handleChange}
-                // dateFormat="dd/MM/yyyy"
-                dateFormat="dd/MM/yyyy"
-                className={classNames}
-                placeholderText={placeholder || "Select a Date"}
-                icon={
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 48 48"
-                    >
-                        <mask id="ipSApplication0">
-                            <g fill="none" stroke="#fff" strokeLinejoin="round" strokeWidth="4">
-                                <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
-                                <path
-                                    fill="#fff"
-                                    d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
-                                ></path>
-                            </g>
-                        </mask>
-                        <path
-                            fill="currentColor"
-                            d="M0 0h48v48H0z"
-                            mask="url(#ipSApplication0)"
-                        ></path>
-                    </svg>
-                }
-            /> : <DatePicker
-                selected={selectedDate}
-                onChange={handleChange}
-                // dateFormat="dd/MM/yyyy"
-                dateFormat="dd/MM/yyyy"
-                className={classNames}
-                placeholderText={placeholder || "Select a Date"}
-
-            />}
+        <div style={{ width: width || "100%" }} className={classNames}>
+            {showDateIcon ? (
+                <DatePicker
+                    value={selectedDate}
+                    onChange={handleChange}
+                    format="dd/MM/yyyy"
+                    className={classNames}
+                    placeholder={placeholder || "Select a Date"}
+                    calendarIcon={
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 48 48"
+                        >
+                            <mask id="ipSApplication0">
+                                <g fill="none" stroke="#fff" strokeLinejoin="round" strokeWidth="4">
+                                    <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
+                                    <path
+                                        fill="#fff"
+                                        d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
+                                    ></path>
+                                </g>
+                            </mask>
+                            <path
+                                fill="currentColor"
+                                d="M0 0h48v48H0z"
+                                mask="url(#ipSApplication0)"
+                            ></path>
+                        </svg>
+                    }
+                />
+            ) : (
+                <DatePicker
+                    value={selectedDate}
+                    onChange={handleChange}
+                    format="dd/MM/yyyy"
+                    className={classNames}
+                    placeholder={placeholder || "Select a Date"}
+                />
+            )}
         </div>
     );
 };
--- src/components/schemas/newMemberFormSchema.mjs
+++ src/components/schemas/newMemberFormSchema.mjs
@@ -2,15 +2,16 @@ import { z } from 'zod';
 
 const memberFormSchema = z.object({
     name: z.string().min(1, "নাম লাগবে।"),
-    type: z.boolean(),
-    father: z.string().min(1, "পিতার নাম দেয়া হয়নি।"),
-    mother: z.string().min(1, "মায়ের নাম দেয়া হয়নি।"),
-    village: z.string().min(1, "গ্রাম উল্লেখ করুন।"),
-    post: z.string().min(1, "পোস্ট অফিস দিন।"),
-    policeStation: z.string().min(1, "থানা উল্লেখ করুন"),
-    district: z.string().min(1, "জেলা দিন"),
-    mobileNumber: z.string().min(11, "মোবাইল নাম্বার ১১ ডিজিটের হতে হবে।"),
-    email: z.string().email("সঠিক ই-ইমেইল দিন"),
-    nationalId: z.string().min(10, "জাতীয় পরিচয়পত্র উল্লেখ করুন।"),
-  });
-export default memberFormSchema  
+    type: z.boolean().optional(),
+    father: z.string().optional(),
+    mother: z.string().optional(),
+    village: z.string().optional(),
+    post: z.string().optional(),
+    policeStation: z.string().optional(),
+    district: z.string().optional(),
+    mobileNumber: z.string().optional(),
+    email: z.string().optional(),
+    nationalId: z.string().optional(),
+});
+
+export default memberFormSchema;
